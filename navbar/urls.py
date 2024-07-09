d7e9ddd33e142978e887e488f79c6f463c5f5a05
xurshidbeek <129169853+xurshidbeek@users.noreply.github.com>
2024-07-09 16:16:26 +0500

    commit

--- navbar/urls.py
+++ navbar/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import home, books, author
+from .views import home, books, author, book_detail
 
 urlpatterns = [
     path('', home, name='home'),
     path('book/', books, name='books'),
     path('auth/', author, name='author'),
-
+    path('book/<int:id>/', book_detail, name='book_detail'),
 ]
 
 if settings.DEBUG:
--- navbar/views.py
+++ navbar/views.py
@@ -30,3 +30,16 @@ def author(request):
     author = Authors.objects.all()
     context = {'author':author}
     return render(request, 'author.html', context=context)
+
+
+def book_detail(request, id):
+    if request.method == 'POST':
+        search = request.POST['search']
+        books = Books.objects.filter(tittle__icontains=search) | Books.objects.filter(
+            author__first_name__icontains=search)
+        if books:
+            return render(request, 'book.html', {'book': books, "value": search, "message": "Succesfuly"})
+        else:
+            return render(request, 'book.html', {'message': "NOt found"})
+    book = Books.objects.get(id=id)
+    return render(request, 'book_detail.html', {'book': book})
